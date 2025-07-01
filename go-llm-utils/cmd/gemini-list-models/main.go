2e6595c136076200205a362e0a00e066eefb001e
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2025-07-01 15:25:05 +0200

    feat: add gemini-list-models module

---
+++ go-llm-utils/cmd/gemini-list-models/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"slices"
+
+	"github.com/softwaredevelop/prompt-engineering/go-llm-utils/pkg/gemini"
+)
+
+func main() {
+	ctx := context.Background()
+	client, err := gemini.NewGenAIClient(ctx)
+	if err != nil {
+		log.Fatalf("client error: %v", err)
+	}
+
+	lister := &gemini.GenAIModelLister{Client: client}
+	models, err := gemini.ListModels(ctx, lister)
+
+	fmt.Println("List of models that support generateContent:")
+	for _, model := range models.Items {
+		if slices.Contains(model.SupportedActions, "generateContent") {
+			fmt.Println(model.Name)
+		}
+	}
+}
