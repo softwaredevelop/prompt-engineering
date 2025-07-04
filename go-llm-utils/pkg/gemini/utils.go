bffcac21542fc65b4f75538ee85919e9f131f1b9
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2025-07-04 18:12:23 +0200

    refactor: add PrintResponse function

--- go-llm-utils/pkg/gemini/utils.go
+++ go-llm-utils/pkg/gemini/utils.go
@@ -1,5 +1,21 @@
 //revive:disable:package-comments,exported
 package gemini
 
+import (
+	"fmt"
+
+	"google.golang.org/genai"
+)
+
 func F32(v float32) *float32 { return &v }
 func I32(v int32) *int32     { return &v }
+
+func PrintResponse(resp *genai.GenerateContentResponse) {
+	for _, cand := range resp.Candidates {
+		if cand.Content != nil {
+			for _, part := range cand.Content.Parts {
+				fmt.Println(part.Text)
+			}
+		}
+	}
+}
